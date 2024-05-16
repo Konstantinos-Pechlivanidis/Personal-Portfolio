6868d709495792fa1f6fe406d987fecdb0f35ccd
kwstas.pexli <77242799+true-hero23@users.noreply.github.com>
2024-05-16 16:21:28 +0300

    Update toggleButton (language) - Zoho PageSense Test

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,6 +43,12 @@ const englishContent = document.querySelectorAll('[language="English"]');
 let isGreek = true;
 
 toggleButton.addEventListener("click", function () {
+  // Zoho Pagesense (Test)
+  var activityName = 'key';
+  var activityJSON = {'value': 1};
+  window.pagesense = window.pagesense || [];
+  window.pagesense.push(['trackActivity', activityName, activityJSON]);
+  
   const mobileNav = document.querySelector(".navbar.active");
   isGreek = !isGreek;
   isGreek
